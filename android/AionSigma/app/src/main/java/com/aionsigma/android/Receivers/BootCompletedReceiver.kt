747416ae7cf1374dc7d443930c48b621c996980a
huybuiquang <huybuiquang@gmail.com>
2018-09-16 10:53:51 +0700

    add receiver BOOT_COMPLETED

---
+++ android/AionSigma/app/src/main/java/com/aionsigma/android/Receivers/BootCompletedReceiver.kt
@@ -0,0 +1,17 @@
+package com.aionsigma.android.Receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+import com.aionsigma.android.Services.SyncDataService
+
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if ("android.intent.action.BOOT_COMPLETED" == intent.action) {
+            val pushIntent = Intent(context, SyncDataService::class.java)
+            context.startService(pushIntent)
+        }
+    }
+}

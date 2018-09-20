89b983e8c2194d5d7fe93b5ca4052bca404b86f5
Tín Hồ <htritin@gmail.com>
2018-09-21 00:35:30 +0700

    Test get call log

--- android/AionSigma/app/src/main/java/com/aionsigma/android/Controller/WelcomeActivity.kt
+++ android/AionSigma/app/src/main/java/com/aionsigma/android/Controller/WelcomeActivity.kt
@@ -1,20 +1,106 @@
 package com.aionsigma.android.Controller
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.aionsigma.android.R
+import android.provider.CallLog
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import java.util.*
+
 
 class WelcomeActivity : AppCompatActivity() {
+
+    val MY_PERMISSIONS_REQUEST_READ_CALL_LOG = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_CALL_LOG)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_CALL_LOG),
+                        MY_PERMISSIONS_REQUEST_READ_CALL_LOG)
+            }
+        } else {
+            val callLog = getCallDetails();
+            Log.v("CallLog", callLog);
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_CALL_LOG -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay!
+                    val callLog = getCallDetails();
+                    Log.v("CallLog", callLog);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
     fun btnCreateCircleOnClicked(view: View){
         var loginIntent = Intent(this,LoginActivity::class.java)
         startActivity(loginIntent)
+    }
+
+    private fun getCallDetails(): String {
+
+        val sb = StringBuffer()
+        val managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null)
+        val number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER)
+        val type = managedCursor.getColumnIndex(CallLog.Calls.TYPE)
+        val date = managedCursor.getColumnIndex(CallLog.Calls.DATE)
+        val duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION)
+        sb.append("Call Details :")
+        while (managedCursor.moveToNext()) {
+            val phNumber = managedCursor.getString(number)
+            val callType = managedCursor.getString(type)
+            val callDate = managedCursor.getString(date)
+            val callDayTime = Date(java.lang.Long.valueOf(callDate))
+            val callDuration = managedCursor.getString(duration)
+            var dir: String? = null
+            val dircode = Integer.parseInt(callType)
+            when (dircode) {
+                CallLog.Calls.OUTGOING_TYPE -> dir = "OUTGOING"
+
+                CallLog.Calls.INCOMING_TYPE -> dir = "INCOMING"
+
+                CallLog.Calls.MISSED_TYPE -> dir = "MISSED"
+            }
+            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+                    + dir + " \nCall Date:--- " + callDayTime
+                    + " \nCall duration in sec :--- " + callDuration)
+            sb.append("\n----------------------------------")
+        }
+        managedCursor.close()
+        return sb.toString()
     }
 }

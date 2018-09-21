8b5f793fe09de0d88ee5d3476046b82936b36ae0
huybuiquang <huybuiquang@gmail.com>
2018-09-21 21:39:39 +0700

    Update MyProfile

--- android/AionSigma/app/src/main/java/com/aionsigma/android/Controller/MainFragments/MyProfileFragment.kt
+++ android/AionSigma/app/src/main/java/com/aionsigma/android/Controller/MainFragments/MyProfileFragment.kt
@@ -3,11 +3,14 @@ package com.aionsigma.android.Controller.MainFragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.aionsigma.android.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,11 +23,27 @@ private const val ARG_PARAM2 = "param2"
  */
 class MyProfileFragment : Fragment() {
 
+    val TAG : String = "MyProfileFragment"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        var view = inflater.inflate(R.layout.fragment_my_profile, container, false)
+
+        val myProfile_btnAdd = view.findViewById<at.markushi.ui.CircleButton>(R.id.myProfile_btnAdd)
+//        myProfile_btnAdd.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(v: View) {
+//                Log.d(TAG,"myProfile_btnAdd.setOnClickListener")
+//            }
+//        })
+        myProfile_btnAdd.setOnClickListener{view->
+            Log.d(TAG,"myProfile_btnAdd.setOnClickListener")
+            Log.d(TAG,"myProfile_btnAdd.setOnClickListener1")
+        }
+        return view
     }
 
 
+
+
 }
+

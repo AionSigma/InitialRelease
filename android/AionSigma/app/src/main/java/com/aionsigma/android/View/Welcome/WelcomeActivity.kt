9985aea5724828baaa2e6d8b2c1f65e68abfa647
huybuiquang <huybuiquang@gmail.com>
2018-10-03 17:26:37 +0700

    Comment code on Welcome View

--- android/AionSigma/app/src/main/java/com/aionsigma/android/View/Welcome/WelcomeActivity.kt
+++ android/AionSigma/app/src/main/java/com/aionsigma/android/View/Welcome/WelcomeActivity.kt
@@ -34,60 +34,60 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        loadContacts.setOnClickListener { loadContacts() }
+//        loadContacts.setOnClickListener { loadContacts() }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.READ_CALL_LOG)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_CALL_LOG),
-                        MY_PERMISSIONS_REQUEST_READ_CALL_LOG)
-            }
-        } else {
-            val callLog = getCallDetails();
-            Log.v("CallLog", callLog);
-        }
-        val locationutil = LocationUtils(this);
-        Log.v("Location", locationutil.location.toString());
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            // Permission is not granted
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                            Manifest.permission.READ_CALL_LOG)) {
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//            } else {
+//                // No explanation needed, we can request the permission.
+//                ActivityCompat.requestPermissions(this,
+//                        arrayOf(Manifest.permission.READ_CALL_LOG),
+//                        MY_PERMISSIONS_REQUEST_READ_CALL_LOG)
+//            }
+//        } else {
+//            val callLog = getCallDetails();
+//            Log.v("CallLog", callLog);
+//        }
+//        val locationutil = LocationUtils(this);
+//        Log.v("Location", locationutil.location.toString());
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_READ_CALL_LOG -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay!
-                    val callLog = getCallDetails();
-                    Log.v("CallLog", callLog);
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return
-            }
-            PERMISSIONS_REQUEST_READ_CONTACTS -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    loadContacts()
-                } else {
-                    //  toast("Permission must be granted in order to display contacts information")
-                }
-            }
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
-        }
-    }
-
+//    override fun onRequestPermissionsResult(requestCode: Int,
+//                                            permissions: Array<String>, grantResults: IntArray) {
+//        when (requestCode) {
+//            MY_PERMISSIONS_REQUEST_READ_CALL_LOG -> {
+//                // If request is cancelled, the result arrays are empty.
+//                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//                    // permission was granted, yay!
+//                    val callLog = getCallDetails();
+//                    Log.v("CallLog", callLog);
+//                } else {
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+//                return
+//            }
+//            PERMISSIONS_REQUEST_READ_CONTACTS -> {
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    loadContacts()
+//                } else {
+//                    //  toast("Permission must be granted in order to display contacts information")
+//                }
+//            }
+//            // Add other 'when' lines to check for other
+//            // permissions this app might request.
+//            else -> {
+//                // Ignore all other requests.
+//            }
+//        }
+//    }
+//
     fun btnCreateCircleOnClicked(view: View){
         //Get UserInfo
         val userInfo = SharedPreferencesUtils.readUserLogin(this)
@@ -100,88 +100,88 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(loginIntent)
         }
     }
-
-    private fun getCallDetails(): String {
-
-        val sb = StringBuffer()
-        val managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null)
-        val number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER)
-        val type = managedCursor.getColumnIndex(CallLog.Calls.TYPE)
-        val date = managedCursor.getColumnIndex(CallLog.Calls.DATE)
-        val duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION)
-        sb.append("Call Details :")
-        while (managedCursor.moveToNext()) {
-            val phNumber = managedCursor.getString(number)
-            val callType = managedCursor.getString(type)
-            val callDate = managedCursor.getString(date)
-            val callDayTime = Date(java.lang.Long.valueOf(callDate))
-            val callDuration = managedCursor.getString(duration)
-            var dir: String? = null
-            val dircode = Integer.parseInt(callType)
-            when (dircode) {
-                CallLog.Calls.OUTGOING_TYPE -> dir = "OUTGOING"
-
-                CallLog.Calls.INCOMING_TYPE -> dir = "INCOMING"
-
-                CallLog.Calls.MISSED_TYPE -> dir = "MISSED"
-            }
-            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
-                    + dir + " \nCall Date:--- " + callDayTime
-                    + " \nCall duration in sec :--- " + callDuration)
-            sb.append("\n----------------------------------")
-        }
-        managedCursor.close()
-        return sb.toString()
-    }
-
-    private fun loadContacts() {
-        var builder = StringBuilder()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
-                        Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS),
-                    PERMISSIONS_REQUEST_READ_CONTACTS)
-            //callback onRequestPermissionsResult
-        } else {
-            builder = getContacts()
-            listContacts.text = builder.toString()
-        }
-    }
-
-    private fun getContacts(): StringBuilder {
-        val builder = StringBuilder()
-        val resolver: ContentResolver = contentResolver;
-        val cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null,
-                null)
-
-        if (cursor.count > 0) {
-            while (cursor.moveToNext()) {
-                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
-                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                val phoneNumber = (cursor.getString(
-                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))).toInt()
-
-                if (phoneNumber > 0) {
-                    val cursorPhone = contentResolver.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", arrayOf(id), null)
-
-                    if(cursorPhone.count > 0) {
-                        while (cursorPhone.moveToNext()) {
-                            val phoneNumValue = cursorPhone.getString(
-                                    cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                            builder.append("Contact: ").append(name).append(", Phone Number: ").append(
-                                    phoneNumValue).append("\n\n")
-                            Log.e("Name ===>",phoneNumValue);
-                        }
-                    }
-                    cursorPhone.close()
-                }
-            }
-        } else {
-            //   toast("No contacts available!")
-        }
-        cursor.close()
-        return builder
-    }
+//
+//    private fun getCallDetails(): String {
+//
+//        val sb = StringBuffer()
+//        val managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null)
+//        val number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER)
+//        val type = managedCursor.getColumnIndex(CallLog.Calls.TYPE)
+//        val date = managedCursor.getColumnIndex(CallLog.Calls.DATE)
+//        val duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION)
+//        sb.append("Call Details :")
+//        while (managedCursor.moveToNext()) {
+//            val phNumber = managedCursor.getString(number)
+//            val callType = managedCursor.getString(type)
+//            val callDate = managedCursor.getString(date)
+//            val callDayTime = Date(java.lang.Long.valueOf(callDate))
+//            val callDuration = managedCursor.getString(duration)
+//            var dir: String? = null
+//            val dircode = Integer.parseInt(callType)
+//            when (dircode) {
+//                CallLog.Calls.OUTGOING_TYPE -> dir = "OUTGOING"
+//
+//                CallLog.Calls.INCOMING_TYPE -> dir = "INCOMING"
+//
+//                CallLog.Calls.MISSED_TYPE -> dir = "MISSED"
+//            }
+//            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+//                    + dir + " \nCall Date:--- " + callDayTime
+//                    + " \nCall duration in sec :--- " + callDuration)
+//            sb.append("\n----------------------------------")
+//        }
+//        managedCursor.close()
+//        return sb.toString()
+//    }
+//
+//    private fun loadContacts() {
+//        var builder = StringBuilder()
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+//                        Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+//            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS),
+//                    PERMISSIONS_REQUEST_READ_CONTACTS)
+//            //callback onRequestPermissionsResult
+//        } else {
+//            builder = getContacts()
+//            listContacts.text = builder.toString()
+//        }
+//    }
+//
+//    private fun getContacts(): StringBuilder {
+//        val builder = StringBuilder()
+//        val resolver: ContentResolver = contentResolver;
+//        val cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null,
+//                null)
+//
+//        if (cursor.count > 0) {
+//            while (cursor.moveToNext()) {
+//                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+//                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+//                val phoneNumber = (cursor.getString(
+//                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))).toInt()
+//
+//                if (phoneNumber > 0) {
+//                    val cursorPhone = contentResolver.query(
+//                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//                            null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", arrayOf(id), null)
+//
+//                    if(cursorPhone.count > 0) {
+//                        while (cursorPhone.moveToNext()) {
+//                            val phoneNumValue = cursorPhone.getString(
+//                                    cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+//                            builder.append("Contact: ").append(name).append(", Phone Number: ").append(
+//                                    phoneNumValue).append("\n\n")
+//                            Log.e("Name ===>",phoneNumValue);
+//                        }
+//                    }
+//                    cursorPhone.close()
+//                }
+//            }
+//        } else {
+//            //   toast("No contacts available!")
+//        }
+//        cursor.close()
+//        return builder
+//    }
 }
